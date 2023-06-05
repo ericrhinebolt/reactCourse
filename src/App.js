68274b5310564d0a505af93b5dcda52846a1874d
Eric Rhinebolt <erhinebo@gmail.com>
2023-06-05 15:39:13 -0400

    add keys, delete funtionality, and conditional rendering on form

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import NewTodoForm from "./components/NewTodoForm";
 
 function App() {
 
+    const [showAddTodoForm, setShowAddTodoForm] = useState(false);
+
     const [todos, setTodos] = useState([
         {rowNumber: 1, rowDescription: 'Feed puppy', rowAssigned: 'User One'},
         {rowNumber: 2, rowDescription: 'Water plants', rowAssigned: 'User Two'},
@@ -13,14 +15,26 @@ function App() {
     ])
 
     const addTodo = (description, assigned) => {
+        let rowNumber = 0;
         if (todos.length > 0) {
+            rowNumber = todos[todos.length - 1].rowNumber + 1;
+        } else {
+            rowNumber = 1;
+        }
             const newTodo = {
-                rowNumber: todos.length + 1,
+                rowNumber: rowNumber,
                 rowDescription: description,
                 rowAssigned: assigned
             };
             setTodos(todos => [...todos, newTodo])
-        }
+
+    }
+
+    const deleteTodo = (deleteTodoRowNumber) => {
+        let filtered = todos.filter(function (value) {
+            return value.rowNumber !== deleteTodoRowNumber;
+        });
+        setTodos(filtered);
     }
 
   return (
@@ -30,11 +44,13 @@ function App() {
                 Your Todo's
             </div>
             <div className='card-body'>
-                <TodoTable todos={todos}/>
-                <button className='btn btn-primary' onClick={addTodo}>
-                    Add new todo
+                <TodoTable todos={todos} deleteTodo={deleteTodo}/>
+                <button onClick={() => setShowAddTodoForm(!showAddTodoForm)} className='btn btn-primary'>
+                    {showAddTodoForm ? 'Close New Todo' : 'New Todo'}
                 </button>
-                <NewTodoForm addTodo={addTodo}/>
+                {showAddTodoForm &&
+                    <NewTodoForm addTodo={addTodo}/>
+                }
             </div>
         </div>
     </div>
